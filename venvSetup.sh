5bcb4d3eb1fec7571dcb9ef290a8f1dd44e23d99
S-Kathirvel <kathirvel.shanmugam.s@gmail.com>
2024-04-18 12:02:48 +0530

    Virtual environment setup

---
+++ venvSetup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Create a virtual environment named "pdf_qa_env"
+python -m venv pdf_qa_env
+
+# Activate the virtual environment (source for bash, .\ for PowerShell)
+source pdf_qa_env/bin/activate
