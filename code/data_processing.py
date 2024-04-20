8c30cea2851ca834ee8c0f459e8c75c1e9989c11
S-Kathirvel <kathirvel.shanmugam.s@gmail.com>
2024-04-21 00:10:38 +0530

    Changed from SmartPDF(llmsherpa) to LlamaIndex reader

--- code/data_processing.py
+++ code/data_processing.py
@@ -1,4 +1,6 @@
-from llama_index.readers.smart_pdf_loader import SmartPDFLoader
+# from llama_index.readers.smart_pdf_loader import SmartPDFLoader
+from llama_index.core import SimpleDirectoryReader
+
 
 # Define LLMSherpa API URL (if applicable)
 llmsherpa_api_url = "https://readers.llmsherpa.com/api/document/developer/parseDocument?renderFormat=all"
@@ -14,14 +16,16 @@ def load_data(data_path):
   Returns:
       list: A list of documents loaded from the data source.
   """
-  if data_path.endswith(".pdf"):
-    pdf_loader = SmartPDFLoader(llmsherpa_api_url=llmsherpa_api_url)
-    document = pdf_loader.load_data(data_path)
-    return [document]
-  else:
+  # if data_path.endswith(".pdf"):
+  #   pdf_loader = SmartPDFLoader(llmsherpa_api_url=llmsherpa_api_url)
+  #   document = pdf_loader.load_data(data_path)
+  #   return [document]
+  # else:
     # Implement logic for loading documents from a directory using SimpleDirectoryReader (not shown here)
-    pass
+  reader = SimpleDirectoryReader(data_path)
+  documents = reader.load_data()
+  return documents
 
 # Example usage (assuming you have a PDF file named "Twixor.pdf")
-pdf_url = "/path/to/your/pdf/file"
+pdf_url = "F:/Pdf_QA/data/"
 documents = load_data(pdf_url)
